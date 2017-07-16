8169545061fa6df0a7faa578c2d35d2e9e753433
alexmalinskiy <alexmalinskiy@gmail.com>
2017-07-16 23:17:15 +0400

    minor improvement

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 # folder SOurce and convert.exe lays in the same dir as python file
-def convert_file():
-    import subprocess
-    import os.path
+import subprocess
+import os.path
+import os
 
+
+def convert_file():
     file_path = os.getcwd()
     images_path = os.path.join(file_path, "Source")
+    result_path = os.path.join(images_path, "Result")
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
     files = os.listdir(images_path)
 
     for file in files:
         if os.path.isfile(os.path.join(images_path, file)):
-            command = "convert " + os.path.join("Source", file) + " -resize 200 " + \
-                      os.path.join("Source", "Result", file.replace(".jpg", "(new).jpg"))
-            proc_to_run = os.path.join(file_path, command)
-            process = subprocess.run(proc_to_run)
+            command = "convert " + os.path.join(os.path.relpath(images_path), file) + " -resize 200 " + \
+                      os.path.join(os.path.relpath(result_path), file.replace(".jpg", "(new).jpg"))
+
+            process = subprocess.run(command)
             if process.returncode == 0:
                 print("{0} successfully converted".format(file))
 
 
-convert_file()
+if __name__ == '__main__':
+    convert_file()
